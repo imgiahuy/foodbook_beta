0fd058e80a3d753ee9dbcf9662b7a91e45c0f5f5
imgiahuy <nguyenphatgiahuy1101@gmail.com>
2025-06-18 14:32:23 +0200

    get_started_page in progress

--- lib/features/on_boarding/presentation/on_boarding_screen.dart
+++ lib/features/on_boarding/presentation/on_boarding_screen.dart
@@ -8,6 +8,7 @@ class OnBoardingScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        //colorScheme define later, with this get started we only use once
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         brightness: Brightness.light,
       ),
@@ -19,6 +20,15 @@ class OnBoardingScreen extends StatelessWidget {
           bottom: false,
           child: Stack(
             children: [
+              Positioned(
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/food_plate.png',
+                    fit: BoxFit.cover,
+                    height: 420,
+                  ),
+                ),
+              ),
               Positioned(
                 top: 20,
                 left: 0,
@@ -43,17 +53,33 @@ class OnBoardingScreen extends StatelessWidget {
               Positioned(
                 right: -5,
                 bottom: -110,
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      child: Image.asset(
-                        'assets/images/get_started_thinking_freebg.png',
-                        height: 700,
-                        width: 300,
-                        fit: BoxFit.cover,
+                child: SizedBox(
+                  child: Image.asset(
+                    'assets/images/get_started_thinking_freebg.png',
+                    height: 700,
+                    width: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 50,
+                right: 50,
+                bottom: 50,
+                child: SizedBox(
+                  height: 70,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Get started",
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromARGB(255, 250, 74, 12),
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
